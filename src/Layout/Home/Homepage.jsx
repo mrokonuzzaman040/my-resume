e83158495fed17e9d3b397d49189fea179eeda7d
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-12-10 01:10:17 +0600

    Add Footer and Outlet components to Homepage

--- src/Layout/Home/Homepage.jsx
+++ src/Layout/Home/Homepage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
+import { Outlet } from 'react-router-dom';
 
 const Homepage = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
