f725056563d3393a4771f97cd6e44f2bfa4de0c0
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-12-11 04:25:51 +0600

    Refactor JSX formatting in MyProject component

--- src/Layout/Home/MyProject/MyProject.jsx
+++ src/Layout/Home/MyProject/MyProject.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const MyProject = () => {
     return (
-        <div className='w-full max-w-[85rem] mx-auto mt-10'>
+        <div className="w-full max-w-[85rem] mx-auto mt-10">
             <div className="mb-10">
-                <h2 className='text-2xl font-black'>Projects</h2>
-                <h1 className='uppercase text-4xl text-gray-300'>MY CREATION</h1>
+                <h2 className="text-2xl font-black">Projects</h2>
+                <h1 className="uppercase text-4xl text-gray-300">MY CREATION</h1>
             </div>
-
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-                
+                <h2></h2>
             </div>
         </div>
     );
