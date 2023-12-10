3937e931a1b27acd577e5fd9a5c92d314012b242
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-12-11 02:54:39 +0600

    Update Skills component with new icons and headings

--- src/Layout/Home/Skills/Skills.jsx
+++ src/Layout/Home/Skills/Skills.jsx
@@ -5,7 +5,7 @@ import react from '../../../image/icons/react.png';
 import mongodb from '../../../image/icons/mogo-db.png';
 import expressjs from '../../../image/icons/expressjs.png';
 import firebase from '../../../image/icons/firebase.png';
-import jwr from '../../../image/icons/jwt.png';
+import jwt from '../../../image/icons/jwt.png';
 import nodejs from '../../../image/icons/nodejs.png';
 import python from '../../../image/icons/python.png';
 import reactrouter from '../../../image/icons/react-router.png';
@@ -13,7 +13,11 @@ import reactrouter from '../../../image/icons/react-router.png';
 const Skills = () => {
     return (
         <div className='w-full max-w-[85rem] sm:px-6 lg:px-8 mx-auto'>
-            <div className="grid grid-cols-3 grid-rows-2 gap-8">
+            <div className="mb-10">
+                <h2 className='text-2xl font-black'>Skills</h2>
+                <h1 className='uppercase text-4xl text-gray-300'>My Top Skills</h1>
+            </div>
+            <div className="grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-4 sm:grid-rows-1 lg:grid-rows-2 gap-8">
                 <div className="flex items-center gap-3">
                     <div className="">
                         <img className='w-24 h-22' src={react} alt="" />
@@ -37,7 +41,7 @@ const Skills = () => {
                         <img className='w-24 h-22' src={nodejs} alt="" />
                     </div>
                     <div className="">
-                        <h2 className='font-bold uppercase text-xl'>MongoDB</h2>
+                        <h2 className='font-bold uppercase text-xl'>Nodejs</h2>
                         <p className='text-gray-500 text-sm'>Lorem,consectetur adipisicing elit. Totam, culpa?</p>
                     </div>
                 </div>
@@ -46,7 +50,7 @@ const Skills = () => {
                         <img className='w-24 h-22' src={firebase} alt="" />
                     </div>
                     <div className="">
-                        <h2 className='font-bold uppercase text-xl'>MongoDB</h2>
+                        <h2 className='font-bold uppercase text-xl'>Firebase</h2>
                         <p className='text-gray-500 text-sm'>Lorem,consectetur adipisicing elit. Totam, culpa?</p>
                     </div>
                 </div>
@@ -55,7 +59,7 @@ const Skills = () => {
                         <img className='w-24 h-22' src={reactrouter} alt="" />
                     </div>
                     <div className="">
-                        <h2 className='font-bold uppercase text-xl'>MongoDB</h2>
+                        <h2 className='font-bold uppercase text-xl'>React Router</h2>
                         <p className='text-gray-500 text-sm'>Lorem,consectetur adipisicing elit. Totam, culpa?</p>
                     </div>
                 </div>
@@ -64,7 +68,25 @@ const Skills = () => {
                         <img className='w-24 h-22' src={expressjs} alt="" />
                     </div>
                     <div className="">
-                        <h2 className='font-bold uppercase text-xl'>MongoDB</h2>
+                        <h2 className='font-bold uppercase text-xl'>ExpressJS</h2>
+                        <p className='text-gray-500 text-sm'>Lorem,consectetur adipisicing elit. Totam, culpa?</p>
+                    </div>
+                </div>
+                <div className="flex items-center gap-3">
+                    <div className="">
+                        <img className='w-24 h-22' src={jwt} alt="" />
+                    </div>
+                    <div className="">
+                        <h2 className='font-bold uppercase text-xl'>JWR</h2>
+                        <p className='text-gray-500 text-sm'>Lorem,consectetur adipisicing elit. Totam, culpa?</p>
+                    </div>
+                </div>
+                <div className="flex items-center gap-3">
+                    <div className="">
+                        <img className='w-24 h-22' src={python} alt="" />
+                    </div>
+                    <div className="">
+                        <h2 className='font-bold uppercase text-xl'>Python</h2>
                         <p className='text-gray-500 text-sm'>Lorem,consectetur adipisicing elit. Totam, culpa?</p>
                     </div>
                 </div>
