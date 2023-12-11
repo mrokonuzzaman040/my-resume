fe7a871bf867b2dc3739c7aef7a9864c93f226ad
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-12-11 15:59:23 +0600

    Add images and links to project cards

--- src/Layout/Home/MyProject/MyProject.jsx
+++ src/Layout/Home/MyProject/MyProject.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+// images
+import bdhomefindes from '../../../image/project/bdhomefinder.png'
+import officetime from '../../../image/project/office-time.png'
+import carhat from '../../../image/project/car-hat.png'
 
 const MyProject = () => {
     return (
@@ -8,18 +14,48 @@ const MyProject = () => {
                 <h1 className="uppercase text-4xl text-gray-300">MY CREATION</h1>
             </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-                <div className="flex flex-col gap-3 glass rounded-xl bg-slate-50 p-4">
-                    <div className="flex justify-between items-center">
-                        <h2 className="font-bold uppercase text-xl">Ecommerce Website</h2>
-                        <a
-                            href=""
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="text-gray-500 hover:text-indigo-500"
-                        >
-                            <FaExternalLinkAlt className="text-xl"></FaExternalLinkAlt>
-                        </a>
-                        
+                <div className="relative overflow-hidden rounded-lg shadow-lg group">
+                    <div className="absolute inset-0 z-10 transition duration-300 bg-black bg-opacity-50 group-hover:bg-opacity-0"></div>
+                    <img
+                        className="object-cover w-full h-64 transition duration-300 transform group-hover:scale-110"
+                        src={bdhomefindes}
+                        alt=""
+                    />
+                    <div className="absolute inset-0 flex items-center justify-center z-20">
+                        <div className="text-center">
+                            <h3 className="text-lg font-bold text-white">bdHomeFinders</h3>
+                            <p className="text-sm text-white">Get your dream home here.</p>
+                        </div>
+                    </div> 
+                </div>
+                <div className="relative overflow-hidden rounded-lg shadow-lg group">
+                    <div className="absolute inset-0 z-10 transition duration-300 bg-black bg-opacity-50 group-hover:bg-opacity-0"></div>
+                    <img
+                        className="object-cover w-full h-64 transition duration-300 transform group-hover:scale-110"
+                        src={officetime}
+                        alt=""
+                    />
+                    <div className="absolute inset-0 flex items-center justify-center z-20">
+                        <div className="text-center">
+                            <h3 className="text-lg font-bold text-white">Office Time</h3>
+                            <p className="text-sm text-white">Corporate event management system.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="relative overflow-hidden rounded-lg shadow-lg group">
+                    <div className="absolute inset-0 z-10 transition duration-300 bg-black bg-opacity-50 group-hover:bg-opacity-0"></div>
+                    <img
+                        className="object-cover w-full h-64 transition duration-300 transform group-hover:scale-110"
+                        src={carhat}
+                        alt=""
+                    />
+                    <div className="absolute inset-0 flex items-center justify-center z-20">
+                        <div className="text-center">
+                            <h3 className="text-lg font-bold text-white">Car Hat</h3>
+                            <p className="text-sm text-white">Online car dealer point.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
