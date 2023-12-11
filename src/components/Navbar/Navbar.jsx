a9e8cff5344572b52e0eb50a11d1c78ab327c043
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-12-12 05:02:01 +0600

    Refactor Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,58 +1,44 @@
 import React from 'react';
 import logo from '../../assets/fabicon.svg'
 import { Link, NavLink } from 'react-router-dom';
+
+const link = <>
+    <NavLink to={'/'} className={({ isActive, isPending }) =>
+        isPending ? "pending" : isActive ? "text-indigo-500 font-bold text-sm border-b-4 border-b-indigo-500" : "font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"}>Home</NavLink>
+
+    <NavLink to={'/about'} className={({ isActive, isPending }) =>
+        isPending ? "pending" : isActive ? "text-indigo-500 font-bold text-sm border-b-4 border-b-indigo-500" : "font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"}>About Me</NavLink>
+
+    <NavLink to={'/contact'} className={({ isActive, isPending }) =>
+        isPending ? "pending" : isActive ? "text-indigo-500 font-bold text-sm border-b-4 border-b-indigo-500" : "font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"} href="#">Contact</NavLink>
+</>
 const Navbar = () => {
     return (
-        <div className="">
-            <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-gray-900 text-sm py-4">
-                <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
-                    <div className="flex items-center justify-between">
-                        <Link to={'/'} className="inline-flex items-center gap-x-2 text-xl font-semibold text-indigo-600">
-                            <img className="w-10 h-auto" src={logo} alt="Logo" />
-                            Rokon
-                        </Link>
-                        
-                        <div className="sm:hidden">
-                            <button type="button" className="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-gray-700 dark:text-white dark:hover:bg-white/10 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" data-hs-collapse="#navbar-image-and-text-2" aria-controls="navbar-image-and-text-2" aria-label="Toggle navigation">
-                                <svg className="hs-collapse-open:hidden flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" x2="21" y1="6" y2="6" /><line x1="3" x2="21" y1="12" y2="12" /><line x1="3" x2="21" y1="18" y2="18" /></svg>
-                                <svg className="hs-collapse-open:block hidden flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
-                            </button>
+        <>
+            <div className="navbar bg-base-100 w-full max-w-[85rem] sm:px-6 lg:px-8 mx-auto">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
+                        <ul tabIndex={0} className="menu menu-sm flex flex-col gap-3 dropdown-content mt-3 z-[1] p-8 shadow bg-base-100 rounded w-52 ">
+                            {
+                                link
+                            }
+                        </ul>
                     </div>
-
-                    <div id="navbar-image-and-text-2" className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block">
-                        <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
-
-                            <NavLink to={'/'} className={({ isActive, isPending }) =>
-                                isPending ? "pending" : isActive ? "text-indigo-500 font-bold text-sm border-b-4 border-b-indigo-500" : "font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"}>Home</NavLink>
-
-                            <NavLink to={'/about'} className={({ isActive, isPending }) =>
-                                isPending ? "pending" : isActive ? "text-indigo-500 font-bold text-sm border-b-4 border-b-indigo-500" : "font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"}>About Me</NavLink>
-
-                            <NavLink to={'/contact'} className={({ isActive, isPending }) =>
-                                isPending ? "pending" : isActive ? "text-indigo-500 font-bold text-sm border-b-4 border-b-indigo-500" : "font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"} href="#">Contact</NavLink>
-
-                            {/* <NavLink to={'/hireme'} className={({ isActive, isPending }) =>
-                                isPending ? "pending" : isActive ? "text-indigo-500 font-bold text-sm border-b-4 border-b-indigo-500" : "font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"} href="#">Hire Me</NavLink> */}
-
-                            {/* <div className="dropdown mb-72">
-                                <div tabIndex={0} role="button" className="btn m-1">
-                                    Theme
-                                    <svg width="12px" height="12px" className="h-2 w-2 fill-current opacity-60 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048"><path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path></svg>
-                                </div>
-                                <ul tabIndex={0} className="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52">
-                                    <li><input type="radio" name="theme-dropdown" className="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Default" value="default" /></li>
-                                    <li><input type="radio" name="theme-dropdown" className="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Retro" value="retro" /></li>
-                                    <li><input type="radio" name="theme-dropdown" className="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Cyberpunk" value="cyberpunk" /></li>
-                                    <li><input type="radio" name="theme-dropdown" className="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Valentine" value="valentine" /></li>
-                                    <li><input type="radio" name="theme-dropdown" className="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Aqua" value="aqua" /></li>
-                                </ul>
-                            </div> */}
-                        </div>
-                    </div>
-                </nav>
-            </header>
-        </div >
+                    <Link to={'/'} className="inline-flex items-center gap-x-2 text-xl gap-3 font-semibold text-indigo-900">
+                        <img className="w-10 h-auto" src={logo} alt="Logo" />
+                        Rokon
+                    </Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 lg:flex flex-row gap-9">
+                        {link}
+                    </ul>
+                </div>
+            </div>
+        </>
     );
 };
 
