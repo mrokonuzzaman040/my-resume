d1c9994e6c371c655c7d11e039d8a6a2b6908cec
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-12-11 16:15:13 +0600

    Update MyProject.jsx with responsive styling

--- src/Layout/Home/MyProject/MyProject.jsx
+++ src/Layout/Home/MyProject/MyProject.jsx
@@ -8,7 +8,7 @@ import carhat from '../../../image/project/car-hat.png'
 
 const MyProject = () => {
     return (
-        <div className="w-full max-w-[85rem] mx-auto mt-10">
+        <div className="w-full max-w-[85rem] mx-auto mt-10 sm:px-6 lg:px-8">
             <div className="mb-10">
                 <h2 className="text-2xl font-black">Projects</h2>
                 <h1 className="uppercase text-4xl text-gray-300">MY CREATION</h1>
@@ -26,7 +26,7 @@ const MyProject = () => {
                             <h3 className="text-lg font-bold text-white">bdHomeFinders</h3>
                             <p className="text-sm text-white">Get your dream home here.</p>
                         </div>
-                    </div> 
+                    </div>
                 </div>
                 <div className="relative overflow-hidden rounded-lg shadow-lg group">
                     <div className="absolute inset-0 z-10 transition duration-300 bg-black bg-opacity-50 group-hover:bg-opacity-0"></div>
