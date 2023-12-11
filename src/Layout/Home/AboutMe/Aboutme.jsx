a5fcab62c74cce35f810455513dec9341bfec0d6
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-12-12 04:00:14 +0600

    Refactor AboutMe and Footer components

--- src/Layout/Home/AboutMe/Aboutme.jsx
+++ src/Layout/Home/AboutMe/Aboutme.jsx
@@ -1,17 +1,38 @@
-import React from 'react';
-import profilepic from '../../../image/Person/profile.jpg'
+import { useEffect, useRef } from 'react';
+import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
+import profilepic from '../../../image/Person/profile.jpg';
 import Experiance from './Experiance';
 
+
 const Aboutme = () => {
+    const aboutMeRef = useRef(null);
+
+    useEffect(() => {
+        const aboutMeElement = aboutMeRef.current;
+        aboutMeElement.classList.add('fade-in');
+    }, []);
+
     return (
-        <div className='w-full max-w-[85rem] sm:px-6 lg:px-8 mx-auto'>
-            <div className="flex items-center justify-center p-20">
+        <div className='w-full max-w-[85rem] sm:px-6 lg:px-8 mx-auto' ref={aboutMeRef}>
+            <div className="flex items-center justify-center p-10">
                 <img className='h-96 w-96 rounded-full' src={profilepic} alt="" />
+            </div>
+            <div className="flex justify-center gap-4 mb-4">
+                <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin className="text-4xl text-indigo-500 hover:text-indigo-700" />
+                </a>
+                <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+                    <FaGithub className="text-4xl text-indigo-500 hover:text-indigo-700" />
+                </a>
+                <a href="mailto:your-email@example.com">
+                    <FaEnvelope className="text-4xl text-indigo-500 hover:text-indigo-700" />
+                </a>
             </div>
             <div className="text-center flex flex-col gap-4 mb-7">
                 <h1 className='text-4xl'>Hi, I'm <span>Md. Rokon Uzzaman</span></h1>
                 <p>As a passionate and detail-oriented web developer, I possess expertise in ReactJS, NodeJS, ExpressJS, MongoDB, among others. My specialization lies in designing seamless and user-centric web applications that transform ideas into reality.</p>
             </div>
+
             <Experiance />
         </div>
     );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,22 +1,90 @@
 import React from 'react';
 import logo from '../../assets/fabicon.svg'
 import { Link } from 'react-router-dom';
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import { BiLogoUpwork } from "react-icons/bi";
 
 const Footer = () => {
     return (
-        <footer class="w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto">
+        <>
+            <div className="bg-gray-900 text-white">
+                <div className="max-w-7xl mx-auto px-4 py-8">
+                    <div className="grid grid-cols-3 items-center gap-2">
+                        <div className="w-full sm:max-w-sm px-4">
+                            <div className="mb-4">
+                                <img src={logo} alt="" className="w-24" />
+                            </div>
+                            <div className="mb-4">
+                                <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+                            </div>
+                            <div className="flex gap-4 mb-4">
+                                <a href="https://github.com/mrokonuzzaman040" target="_blank" rel="noopener noreferrer">
+                                    <FaGithub className="text-2xl text-gray-400 hover:text-gray-300" />
+                                </a>
+                                <a href="https://www.linkedin.com/in/rokonuzzaman040/" target="_blank" rel="noopener noreferrer">
+                                    <FaLinkedin className="text-2xl text-indigo-400 hover:text-gray-300" />
+                                </a>
+                                <a href="https://twitter.com/rokonuzzaman040" target="_blank" rel="noopener noreferrer">
+                                    <FaTwitter className="text-2xl text-sky-400 hover:text-gray-300" />
+                                </a>
+                                <a href="https://www.upwork.com/freelancers/~0109501d92846b8673" target="_blank" rel="noopener noreferrer">
+                                    <BiLogoUpwork className="text-2xl text-green-400 hover:text-gray-300" />
+                                </a>
+                            </div>
+                        </div>
 
-            <div class="text-center">
-                <div>
-                    <a class="flex-none text-xl font-semibold text-black dark:text-white dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#" aria-label="Brand">Rokon Uzzman</a>
-                </div>
+                        <div className="grid col-span-2 grid-cols-3 p-4 m-2">
+                            <div className="w-full px-4">
+                                <h4 className="text-xl mb-4">Links</h4>
+                                <div className="flex flex-col gap-2">
+                                    <Link to="/" className="hover:text-gray-300">Home</Link>
+                                    <Link to="/about" className="hover:text-gray-300">About</Link>
+                                    <Link to="/contact" className="hover:text-gray-300">Contact</Link>
+                                </div>
+                            </div>
+                            <div className="w-full px-4">
+                                <h4 className="text-xl mb-4">Services</h4>
+                                <div className="flex flex-col gap-2">
+                                    <Link to="/" className="hover:text-gray-300">Web Development</Link>
+                                    <Link to="/" className="hover:text-gray-300">Web Design</Link>
+                                    <Link to="/" className="hover:text-gray-300">Mobile App Development</Link>
+                                </div>
+                            </div>
+                            <div className="w-full">
+                                <h4 className="text-xl mb-4">Contact</h4>
+                                <div className="flex flex-wrap flex-col gap-2">
+                                    <a href="mailto:mdrokonuzzamanmail@gmail.com" className="hover:text-gray-300">
+                                        <span className="text-sm">Email: mdrokonuzzamanmail@gmail.com </span>
+                                        <span className="text-sm">
+                                        </span>
+                                    </a>
+                                    <a href="tel:+8801610830286" className="hover:text-gray-300">
+                                        <span className="text-sm">Phone:(+880) 1610-830286 </span>
+                                        <span className="text-sm">
+                                        </span>
+                                    </a>
+                                    <a href="https://goo.gl/maps/9sVfQxR7W5s" target="_blank" rel="noopener noreferrer" className="hover:text-gray-300">
+                                        <span className="text-sm">Address: Mirpur-6, Dhaka, Bangladesh </span>
+                                        <span className="text-sm">
+                                        </span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
 
-                <div class="mt-3">
-                    <p class="text-gray-500">Contact Via <a class="font-semibold text-blue-600 hover:text-blue-700 dark:text-blue-500 dark:hover:text-blue-400" href="#">Email</a> with me.</p>
-                    <p class="text-gray-500">© Rokon Uzzaman. 2023 All rights reserved.</p>
+                    </div>
                 </div>
             </div>
-        </footer>
+            <div className="bg-gray-800 text-white py-4">
+                <div className="max-w-6xl mx-auto px-4">
+                    <div className="flex flex-col md:flex-row justify-between items-center">
+                        <div className="text-sm text-gray-400">All rights reserved. © 2023</div>
+                        <div className="text-sm text-gray-400">Md. Rokon Uzzaman</div>
+                    </div>
+                </div>
+            </div>
+
+        </>
     );
 };
 
