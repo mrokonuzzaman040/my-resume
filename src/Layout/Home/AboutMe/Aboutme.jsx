8b6420453eaeccbce89538414b232610c5e1e04c
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-12-11 23:20:11 +0600

    Add profile picture and update About Me section

--- src/Layout/Home/AboutMe/Aboutme.jsx
+++ src/Layout/Home/AboutMe/Aboutme.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import profilepic from '../../../image/Person/profile.jpg'
 
 const Aboutme = () => {
     return (
         <div>
-            
+            <div className="flex items-center justify-center p-20">
+                <img className='h-96 w-96 rounded-full' src={profilepic} alt="" />
+            </div>
+            <div className="text-center flex flex-col gap-4">
+                <h1 className='text-4xl'>Hi, I'm <span>Md. Rokon Uzzaman</span></h1>
+                <p>As a passionate and detail-oriented web developer, I possess expertise in ReactJS, NodeJS, ExpressJS, MongoDB, among others. My specialization lies in designing seamless and user-centric web applications that transform ideas into reality.</p>
+            </div>
         </div>
     );
 };
