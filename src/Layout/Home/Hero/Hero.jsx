907e752b38c3d5b4c1c50e57680d7ac260a93990
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-12-12 05:05:37 +0600

    Add Link to Download CV button in Hero component

--- src/Layout/Home/Hero/Hero.jsx
+++ src/Layout/Home/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import Typical from 'react-typical'
 import Lottie from 'react-lottie';
 import loti from '../../../image/Animation - 1702238395000.json'
+import { Link } from 'react-router-dom';
 
 const Hero = () => {
     const defaultOptions = {
@@ -34,7 +35,7 @@ const Hero = () => {
                         />
                     </hp>
                     <div className="">
-                        <button className='btn text-white bg-gradient-to-r from-indigo-500 to-blue-400 hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500'>Download CV</button>
+                        <Link to="https://drive.google.com/file/d/1yR5-0YDo9_rEQL5rdEfyiqJu3VUFi17v/view?usp=sharing" className='btn text-white bg-gradient-to-r from-indigo-500 to-blue-400 hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500'>Download CV</Link>
                     </div>
                 </div>
                 <div className="w-full p-4">
