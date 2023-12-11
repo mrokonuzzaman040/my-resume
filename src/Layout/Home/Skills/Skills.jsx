7aab94585970de438112c7992d078b02c5a3e6fb
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-12-12 04:43:27 +0600

    Update skill descriptions

--- src/Layout/Home/Skills/Skills.jsx
+++ src/Layout/Home/Skills/Skills.jsx
@@ -27,7 +27,7 @@ const Skills = () => {
                     </div>
                     <div className="">
                         <h2 className='font-bold uppercase text-xl'>React</h2>
-                        <p className='text-gray-500 text-sm'>Lorem,consectetur adipisicing elit. Totam, culpa?</p>
+                        <p className='text-gray-500 text-sm'>ReactJavaScript library for UIs. Developed by Facebook.</p>
                     </div>
                     {/* <div className="">
                         <FaArrowRight className='text-2xl text-gray-500 hover:text-indigo-500'></FaArrowRight>
@@ -39,7 +39,7 @@ const Skills = () => {
                     </div>
                     <div className="">
                         <h2 className='font-bold uppercase text-xl'>MongoDB</h2>
-                        <p className='text-gray-500 text-sm'>Lorem,consectetur adipisicing elit. Totam, culpa?</p>
+                        <p className='text-gray-500 text-sm'>MongoDB is a NoSQL database system, using a document-oriented model.</p>
                     </div>
                 </Link>
                 <Link className="flex items-center gap-3 bg-slate-50 rounded-xl glass p-2">
@@ -48,7 +48,7 @@ const Skills = () => {
                     </div>
                     <div className="">
                         <h2 className='font-bold uppercase text-xl'>Nodejs</h2>
-                        <p className='text-gray-500 text-sm'>Lorem,consectetur adipisicing elit. Totam, culpa?</p>
+                        <p className='text-gray-500 text-sm'>Node.js is a runtime environment that executes JavaScript code outside a web browser.</p>
                     </div>
                 </Link>
                 <Link className="flex items-center gap-3 bg-slate-50 rounded-xl glass p-2">
@@ -57,7 +57,7 @@ const Skills = () => {
                     </div>
                     <div className="">
                         <h2 className='font-bold uppercase text-xl'>Firebase</h2>
-                        <p className='text-gray-500 text-sm'>Lorem,consectetur adipisicing elit. Totam, culpa?</p>
+                        <p className='text-gray-500 text-sm'>Firebase is a mobile and web application development platform by Google.</p>
                     </div>
                 </Link>
                 <Link className="flex items-center gap-3 bg-slate-50 rounded-xl glass p-2">
@@ -66,7 +66,7 @@ const Skills = () => {
                     </div>
                     <div className="">
                         <h2 className='font-bold uppercase text-xl'>React Router</h2>
-                        <p className='text-gray-500 text-sm'>Lorem,consectetur adipisicing elit. Totam, culpa?</p>
+                        <p className='text-gray-500 text-sm'>React Router is a library for managing navigation and routing in React applications.</p>
                     </div>
                 </Link>
                 <Link className="flex items-center gap-3 bg-slate-50 rounded-xl glass p-2">
@@ -75,7 +75,7 @@ const Skills = () => {
                     </div>
                     <div className="">
                         <h2 className='font-bold uppercase text-xl'>ExpressJS</h2>
-                        <p className='text-gray-500 text-sm'>Lorem,consectetur adipisicing elit. Totam, culpa?</p>
+                        <p className='text-gray-500 text-sm'>Express.js is a minimal and flexible Node.js web application framework.</p>
                     </div>
                 </Link>
                 <Link className="flex items-center gap-3 bg-slate-50 rounded-xl glass p-2">
@@ -83,8 +83,8 @@ const Skills = () => {
                         <img className='w-16 h-22' src={jwt} alt="" />
                     </div>
                     <div className="">
-                        <h2 className='font-bold uppercase text-xl'>JWR</h2>
-                        <p className='text-gray-500 text-sm'>Lorem,consectetur adipisicing elit. Totam, culpa?</p>
+                        <h2 className='font-bold uppercase text-xl'>JWT</h2>
+                        <p className='text-gray-500 text-sm'>JWT stands for JSON Web Token, a compact, URL-safe means of representing claims between two parties. </p>
                     </div>
                 </Link>
                 <Link className="flex items-center gap-3 bg-slate-50 rounded-xl glass p-2">
@@ -93,7 +93,7 @@ const Skills = () => {
                     </div>
                     <div className="">
                         <h2 className='font-bold uppercase text-xl'>Python</h2>
-                        <p className='text-gray-500 text-sm'>Lorem,consectetur adipisicing elit. Totam, culpa?</p>
+                        <p className='text-gray-500 text-sm'>Python is a high-level, versatile programming language known for readability and simplicity.</p>
                     </div>
                 </Link>
             </div>
