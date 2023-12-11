3cbf11e1a65f2d203337c2df451fc7e36c88848a
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-12-12 02:52:54 +0600

    Add react-tilt package

--- src/Layout/Home/MyProject/MyProject.jsx
+++ src/Layout/Home/MyProject/MyProject.jsx
@@ -1,64 +1,99 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React from 'react';
+import { motion } from 'framer-motion';
+import { FaGithub, FaExternalLinkAlt, FaServer } from 'react-icons/fa';
 
 // images
-import bdhomefindes from '../../../image/project/bdhomefinder.png'
-import officetime from '../../../image/project/office-time.png'
-import carhat from '../../../image/project/car-hat.png'
+import bdHomeFinder from '../../../image/project/bdhomefinder.png';
+import officeTime from '../../../image/project/office-time.png';
+import carHat from '../../../image/project/car-hat.png';
 
 const MyProject = () => {
     return (
-        <div className="w-full max-w-[85rem] mx-auto mt-10 sm:px-6 lg:px-8">
-            <div className="mb-10">
-                <h2 className="text-2xl font-black">Projects</h2>
-                <h1 className="uppercase text-4xl text-gray-300">MY CREATION</h1>
+        <section className="my-project">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="section-title">
+                            <h2>My Project</h2>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-4 col-md-6 col-sm-12">
+                        <div className="single-project">
+                            <div className="project-img">
+                                <img src={bdHomeFinder} alt="bd-home-finder" />
+                            </div>
+                            <div className="project-content">
+                                <h3>BD Home Finder</h3>
+                                <p>BD Home Finder is a real estate website. Where you can find your dream home. You can search your home by location and price. You can also see the details of the home.</p>
+                                <div className="project-link">
+                                    <a href="
+                                    https://bd-home-finder.web.app/" target="_blank" rel="noreferrer">
+                                        <FaExternalLinkAlt />
+                                    </a>
+                                    <a href="" target="_blank" rel="noreferrer">
+                                        <FaServer />
+                                    </a>
+                                    <a href="" target="_blank" rel="noreferrer">
+                                        <FaGithub />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row mt-5 mb-5">
+                    <div className="col-lg-4 col-md-6 col-sm-12">
+                        <div className="single-project">
+                            <div className="project-img">
+                                <img src={officeTime} alt="office-time" />
+                            </div>
+                            <div className="project-content">
+                                <h3>Office Time</h3>
+                                <p>Office Time is a website where you can track your office time. You can also see your total working hours and total earning.</p>
+                                <div className="project-link">
+                                    <a href="https://office-time-9c9a0.web.app/" target="_blank" rel="noreferrer">
+                                        <FaExternalLinkAlt />
+                                    </a>
+                                    <a href="" target="_blank" rel="noreferrer">
+                                        <FaServer />
+                                    </a>
+                                    <a href="" target="_blank" rel="noreferrer">
+                                        <FaGithub />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row mt-5 mb-5">
+                    <div className="col-lg-4 col-md-6 col-sm-12">
+                        <div className="single-project">
+                            <div className="project-img">
+                                <img src={carHat} alt="car-hat" />
+                            </div>
+                            <div className="project-content">
+                                <h3>Car Hat</h3>
+                                <p>Car Hat is a website where you can find your dream car. You can search your car by location and price. You can also see the details of the car.</p>
+                                <div className="project-link">
+                                    <a href="https://car-hat.web.app/" target="_blank" rel="noreferrer">
+                                        <FaExternalLinkAlt />
+                                    </a>
+                                    <a href="" target="_blank" rel="noreferrer">
+                                        <FaServer />
+                                    </a>
+                                    <a href="" target="_blank" rel="noreferrer">
+                                        <FaGithub />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-                <div className="relative overflow-hidden rounded-lg shadow-lg group">
-                    <div className="absolute inset-0 z-10 transition duration-300 bg-black bg-opacity-50 group-hover:bg-opacity-0">
-                    </div>
-                    <img
-                        className="object-cover w-full h-64 transition duration-300 transform group-hover:scale-110"
-                        src={bdhomefindes}
-                        alt=""
-                    />
-                    <div className="absolute inset-0 flex items-center justify-center z-20">
-                        <div className="text-center">
-                            <h3 className="text-lg font-bold text-white">bdHomeFinders</h3>
-                            <p className="text-sm text-white">Get your dream home here.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="relative overflow-hidden rounded-lg shadow-lg group">
-                    <div className="absolute inset-0 z-10 transition duration-300 bg-black bg-opacity-50 group-hover:bg-opacity-0"></div>
-                    <img
-                        className="object-cover w-full h-64 transition duration-300 transform group-hover:scale-110"
-                        src={officetime}
-                        alt=""
-                    />
-                    <div className="absolute inset-0 flex items-center justify-center z-20">
-                        <div className="text-center">
-                            <h3 className="text-lg font-bold text-white">Office Time</h3>
-                            <p className="text-sm text-white">Corporate event management system.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="relative overflow-hidden rounded-lg shadow-lg group">
-                    <div className="absolute inset-0 z-10 transition duration-300 bg-black bg-opacity-50 group-hover:bg-opacity-0"></div>
-                    <img
-                        className="object-cover w-full h-64 transition duration-300 transform group-hover:scale-110"
-                        src={carhat}
-                        alt=""
-                    />
-                    <div className="absolute inset-0 flex items-center justify-center z-20">
-                        <div className="text-center">
-                            <h3 className="text-lg font-bold text-white">Car Hat</h3>
-                            <p className="text-sm text-white">Online car dealer point.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </section >
+
     );
 };
 
